30bc6a37fedbc2ce8bb1a2aaa73cc4ef45beacf7
IvanKhizhnyak <vanja.khizhnjak99@gmail.com>
2020-04-23 15:54:06 +0300

    AppNavbar
    Created AppNavbar with bootstrap components.
    Added: yarn add bootstrap reactstrap uuid react-transition-group

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
 import React from 'react';
 import './App.css';
+import AppNavbar from './components/AppNavbar';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <AppNavbar />
     </div>
   );
 }
