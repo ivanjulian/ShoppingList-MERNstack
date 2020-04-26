49754ec1f79bc35346aa63d8e61fb2cf18738162
IvanKhizhnyak <vanja.khizhnjak99@gmail.com>
2020-04-26 13:33:06 +0300

    favicon and header
    Changed favicon and header

--- client/src/components/AppNavbar.js
+++ client/src/components/AppNavbar.js
@@ -13,7 +13,7 @@ import {
 function AppNavbar() {
   const [isOpen, setIsOpen] = useState(false);
 
-  const toggle = () =>{
+  const toggle = () => {
     setIsOpen(!isOpen);
   }
   return (
@@ -27,8 +27,18 @@ function AppNavbar() {
           <Collapse isOpen={isOpen} navbar >
             <Nav className="ml-auto" navbar>
               <NavItem>
+                <NavLink href="">
+                  Log in
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="">
+                  Sign up
+                </NavLink>
+              </NavItem>
+              <NavItem>
                 <NavLink href="https://ivanjulian.github.io/">
-                  GitHub
+                  About the Author
                 </NavLink>
               </NavItem>
             </Nav>
