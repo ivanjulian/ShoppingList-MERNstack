8247780532f86a97b70446b67f86b820e422ce49
IvanKhizhnyak <vanja.khizhnjak99@gmail.com>
2020-04-28 14:10:37 +0300

    auth Actions
    Created auth Actions

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,20 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import AppNavbar from './components/AppNavbar';
 import ShoppingList from './components/ShoppingList';
 
 import { Provider } from 'react-redux';
-import store from './store'
+import store from './store';
+import {loadUser} from './actions/authActions';
 
 import ItemModal from './components/ItemModal';
 import {Container} from 'reactstrap';
 function App() {
+  useEffect(()=>{
+    store.dispatch(loadUser())
+  }, [])
+
   return (
     <Provider store={store}>
       <div className="App">
