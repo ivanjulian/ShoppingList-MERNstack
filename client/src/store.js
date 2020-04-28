65e073052d755db6401c1d27146050f6dc4de2fb
IvanKhizhnyak <vanja.khizhnjak99@gmail.com>
2020-04-28 13:41:46 +0300

    Auth, Register, Login
    Created types and reducers for Auth, Register, Login

--- client/src/store.js
+++ client/src/store.js
@@ -11,6 +11,7 @@ const store = createStore(
   initialState,
   compose(
     applyMiddleware(...middleware),
+    //Redux dev tools. COMMENT IT BEFORE DEPLOY!
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   ));
 
